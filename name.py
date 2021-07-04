ee983f2ce8c267b09050bf10a775e9bd5fb8ef99
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-04 11:01:28 -0400

    Common functions for strings

--- name.py
+++ name.py
@@ -5,3 +5,14 @@ print("he was " + character_age + " years old.")
 character_name = "George"
 print("He really liked the name " + character_name + ",")
 print("but didn't like being " + character_age + ".")
+
+phrase = "Skydiving"
+
+# Most common python functions
+# You can google for lots more functions
+
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("y"))
+print(phrase.replace("Skydiving", "Scuba Diving"))

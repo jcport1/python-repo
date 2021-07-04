aa9466b32ee644b18e01eccf861d51a4d4167e21
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-04 14:29:36 -0400

    Creates mad libs game

---
+++ mad-libs.py
@@ -0,0 +1,7 @@
+musician = input("Enter a musician: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print(f"{musician} to hear, why hear'st thou {musician} sadly?")
+print(f"Sweet with sweets {noun} not, joy delights in joy.")
+print(f"Why {verb} thou that which tou recievest not gladly")

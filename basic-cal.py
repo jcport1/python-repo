628426713f1d63d7164ae049cc0a75a23842b7ea
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-04 14:21:40 -0400

    Creates basic python calculator

---
+++ basic-cal.py
@@ -0,0 +1,13 @@
+
+# input from user is by default a string
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+# convert strings into numbers
+
+# 1st way: use int, integer so only for whole numbers
+# result = int(num1) + int(num2)
+
+# 2nd way: float is a number that has a decimal
+result = float(num1) + float(num2)
+
+print(result)
---
+++ input.py
@@ -0,0 +1,6 @@
+# tells python to ask user for input
+# store input in a variable
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello " + name + "! You are " + age + ".")

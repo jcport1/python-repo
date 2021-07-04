c39a9be0c7372ad692b0d6ca8953f10cd877bb36
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-04 14:08:55 -0400

    Basic python arithmetic

---
+++ number.py
@@ -0,0 +1,68 @@
+from math import *
+
+# to access specific math functions, we have to import them
+# called a module
+# go online to search for different math functions
+
+print(2)
+print(2.0987)
+print(-8.0583)
+
+# basic arithmetic 
+
+print(3 * 4.5)
+print(3 + 4.5)
+print(3 / 4.5)
+
+# you can use parathenses for complex mathematical equations
+# to specify order of operations
+
+print(3 * (4.5 + 5))
+
+# modulus operation
+# takes first number divides it by second number
+# spits out the remainder
+
+print(10 % 3)
+
+# we can store numbers in variables
+
+my_num = 5
+
+print(my_num)
+
+# we can covert number into string
+print(str(my_num) + " my favorite number")
+
+# math functions 
+# function is code that does something
+# can perform math operation on number or give us info
+
+# common functions 
+
+second_num = -5
+
+print(abs(second_num))
+
+# to parameters: 1st is number, 2nd is power
+print(pow(3, 2))
+
+# max
+print(max(4, 10))
+
+# min
+print(min(15, 100))
+
+#round
+print(round(3.7))
+
+# imported math functions 
+
+# floor
+print(floor(3.7))
+
+#ceil 
+print(ceil(3.7))
+
+#square root
+print(sqrt(36))
